152b3d62a1cdba2a448e94ec139994d1190a5030
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-09-26 13:03:34 +0530

    comletion handler modified and combine added

--- Assignment/api/ApiService.swift
+++ Assignment/api/ApiService.swift
@@ -6,27 +6,42 @@
 //
 
 import Foundation
+import Combine
 
 class ApiService : NSObject {
     private let baseUrl = ""
     
     private let sourcesURL = URL(string: "https://api.restful-api.dev/objects")!
     
-    func fetchDeviceDetails(completion : @escaping ([DeviceData]) -> ()){
-        URLSession.shared.dataTask(with: sourcesURL) { (data, urlResponse, error) in
-            if let error = error {
-                print("Network error: \(error.localizedDescription)")
-                completion([]) // Return an empty array on network failure
-                return
-            }
-            
-            if let data = data {
-                let jsonDecoder = JSONDecoder()
-                let empData = try! jsonDecoder.decode([DeviceData].self, from: data)
-                if (empData.isEmpty) {
-                    completion([])
+        func fetchDeviceDetails(completion : @escaping ([DeviceData]) -> ()){
+            URLSession.shared.dataTask(with: sourcesURL) { (data, urlResponse, error) in
+                if let error = error {
+                    print("Network error: \(error.localizedDescription)")
+                    completion([]) // Return an empty array on network failure
+                    return
                 }
-            }
-        }.resume()
-    }
+    
+                if let data = data {
+                    let jsonDecoder = JSONDecoder()
+                    let empData = try! jsonDecoder.decode([DeviceData].self, from: data)
+                    if (empData.isEmpty) {
+                        completion([])
+                    }
+                    else{
+                        completion(empData)
+                    }
+                }
+            }.resume()
+    
+    
+    
+        }
+//    func fetchDeviceDetails()->AnyPublisher<[DeviceData], Error>{
+//        return URLSession.shared.dataTaskPublisher(for: sourcesURL)
+//            .map(\.data)
+//            .decode(type: [DeviceData].self, decoder: JSONDecoder())
+//            .receive(on: DispatchQueue.main)
+//            .eraseToAnyPublisher()
+//    }
 }
+  
--- Assignment/vm/ContentViewModel.swift
+++ Assignment/vm/ContentViewModel.swift
@@ -6,19 +6,40 @@
 //
 
 import Foundation
-
+import Combine
 
 class ContentViewModel : ObservableObject {
     
+    private var cancellables = Set<AnyCancellable>()
     private let apiService = ApiService()
     @Published var navigateDetail: DeviceData? = nil
     @Published var data: [DeviceData]? = []
+    
+    
+    
+        func fetchAPI() {
+            apiService.fetchDeviceDetails(completion: { item in
+                self.data = item
+            })
+        }
+    
+//    func fetchAPI(){
+//        apiService.fetchDeviceDetails()
+//            .sink{ completion in
+//                switch completion{
+//                case .finished:
+//                    print("finished")
+//                case .failure:
+//                    print("error loading")
+//                }
+//                
+//            }  receiveValue:{
+//                [weak self] data in
+//                self?.data = data
+//            }
+//            .store(in: &cancellables)
+//    }
 
-    func fetchAPI() {
-        apiService.fetchDeviceDetails(completion: { item in
-            self.data = item
-        })
-    }
     
     func navigateToDetail(navigateDetail: DeviceData) {
         self.navigateDetail = navigateDetail
